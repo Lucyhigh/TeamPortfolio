acc591f6b453ae17c839a219ac408c944f4e6614
kellan0812 <ces08123@gmail.com>
2022-02-14 16:22:14 +0900

    textout

--- teamPortfoilo/MainGame.cpp
+++ teamPortfoilo/MainGame.cpp
@@ -37,7 +37,7 @@ void MainGame::render(void) // 그려줘
 	wsprintf(str, "난 깃허브 토큰이 싫다...");
 	TextOut(getMemDC(), 230, 200, str, strlen(str));
 
-	wsprintf(str, "웨안됌?");
+	wsprintf(str, "이제 됨");
 	TextOut(getMemDC(), 320, 250, str, strlen(str));
 
 	this->getBackBuffer()->render(getHDC()); //백버퍼의 내용을 HDC에 그린다.
