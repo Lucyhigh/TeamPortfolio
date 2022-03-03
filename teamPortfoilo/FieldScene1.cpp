62950a7e3412ba4dd899008efe428276729f5b5e
kellan0812 <ces08123@gmail.com>
2022-03-03 13:03:09 +0900

    field

--- teamPortfoilo/FieldScene1.cpp
+++ teamPortfoilo/FieldScene1.cpp
@@ -7,17 +7,21 @@ FieldScene1::FieldScene1(){
 
 HRESULT FieldScene1::init(void)
 {
+
 	return S_OK;
 }
 
 void FieldScene1::release(void)
 {
+
 }
 
 void FieldScene1::update(void)
 {
+	
 }
 
 void FieldScene1::render(void)
 {
+	
 }
--- teamPortfoilo/FieldScene1.h
+++ teamPortfoilo/FieldScene1.h
@@ -1,9 +1,23 @@
 #pragma once
 #include "GameNode.h"
+#include "Camera.h"
 
 class FieldScene1 :public GameNode
 {
 private:
+	vector<RECT*> _floor;
+
+	Camera* _camera;
+	Image* _mapImage;
+	Image* _objectImage;
+	RECT* floor0;
+	RECT* floor1;
+	RECT* floor2;
+	RECT _ObjectRc;
+
+	float _x, _y;
+	int _count;
+	int _indexA;
 
 public:
 	FieldScene1();
